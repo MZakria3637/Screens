bb3f78b2bb7960229bb1a1b56ea09b619d9eb264
MZakria3637 <zakriamuhammad3637@gmail.com>
2021-11-13 19:12:35 +0500

    Routes added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import Forgot from "./forgot_password/forgot_password";
  import Login from "./login/login";
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ChangePassword from "./change_password/change_password";
 
 // import CreateAcount from "./create_account/createAcount";
 
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />}></Route>
+        <Route path='forgot-password' element={<Forgot/>}></Route>
+        <Route path='change-password' element={<ChangePassword/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
