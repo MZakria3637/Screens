24cc7b9de207cbb428f5f0b3e73910cc10ca8266
MZakria3637 <zakriamuhammad3637@gmail.com>
2021-11-13 23:03:39 +0500

    routes added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import Forgot from "./forgot_password/forgot_password";
  import Login from "./login/login";
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ChangePassword from "./change_password/change_password";
-
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 // import CreateAcount from "./create_account/createAcount";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Login />}></Route>
-        <Route path='forgot-password' element={<Forgot/>}></Route>
-        <Route path='change-password' element={<ChangePassword/>}></Route>
-      </Routes>
+      <Switch>
+        <Route exact path='/' ><Login /></Route>
+        <Route exact path='/forgot-password' ><Forgot /></Route>
+        <Route exact path='/change-password'><ChangePassword /></Route>
+      </Switch>
     </BrowserRouter>
   );
 }
