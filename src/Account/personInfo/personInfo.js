08322797248d54858d4181485fc909f3c83bbc29
MZakria3637 <zakriamuhammad3637@gmail.com>
2021-11-16 16:28:31 +0500

    Person Info Initiated

---
+++ src/Account/personInfo/personInfo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PersonInfo() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default PersonInfo
--- src/Account/sideNav/sidenav.js
+++ src/Account/sideNav/sidenav.js
@@ -2,14 +2,18 @@ import { List, ListItem, ListItemText } from '@mui/material'
 import { Box } from '@mui/system'
 import React from 'react'
 import Avatar from '@mui/material/Avatar';
-import "./sideNav.css"
+import "./sideNav.css";
+import makeStyles from '@material-ui/styles/makeStyles';
 
 function SideNav() {
-    const styles = theme => ({
-        listItemText: {
-            fontSize: '3rem',
-        }
+    const useStyles = makeStyles({
+        active: {
+            '& .MuiTypography-root': {
+                fontWeight: 'bold',
+            },
+        },
     });
+    const classes = useStyles();
     return (
         <Box
             sx={{ width:250}}
@@ -19,10 +23,16 @@ function SideNav() {
             // onKeyDown={toggleDrawer(anchor, false)}
         >
             <List className="row_side">
+                <Avatar alt="Remy Sharp" className="align-self-center" sx={{ width: 60, height: 60 }} />
+                <span className="lead fw-bold align-self-center mb-5">Ali Awan</span>
                 {['Personal Information', 'Shipping Area', 'Change Password',].map((text, index) => (
-                    <ListItem className="mx-3 boldText" button key={text}>
-                        <ListItemText classes={{ primary: styles.listItemText }} primary={text} />
+                    <ListItem className="mx-3 mb-1 boldText"
+                        button
+                        activeClassName={classes.active}
+                        key={text}>
+                        <ListItemText primary={text} />
                     </ListItem>
+                   
                 ))}
                 <button type="button" class="side_btn btn  fw-bold my-4 ">Return Home</button>
             </List>
