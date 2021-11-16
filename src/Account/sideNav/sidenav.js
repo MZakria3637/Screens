36af9ef5ea300ad3d28c672fe1679d2adddf0207
MZakria3637 <zakriamuhammad3637@gmail.com>
2021-11-16 18:21:24 +0500

    side nav adjusted

--- src/Account/sideNav/sidenav.js
+++ src/Account/sideNav/sidenav.js
@@ -1,41 +1,71 @@
-import { List, ListItem, ListItemText } from '@mui/material'
+import { List, ListItem, ListItemText, Typography } from '@mui/material'
 import { Box } from '@mui/system'
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import Avatar from '@mui/material/Avatar';
 import "./sideNav.css";
-import makeStyles from '@material-ui/styles/makeStyles';
+
 
 function SideNav() {
-    const useStyles = makeStyles({
-        active: {
-            '& .MuiTypography-root': {
-                fontWeight: 'bold',
-            },
-        },
-    });
-    const classes = useStyles();
+    useEffect(() => {
+        setpersonInfo(true);
+    }, [])
+    const [PersonInfo, setpersonInfo] = useState(false);
+    const [ShippingArea, setshipping] = useState(false);
+    const [ChangePassword, setChangePassword] = useState(false);
     return (
         <Box
-            sx={{ width:250}}
+            sx={{ width: 250, overflow: 'hidden'}}
             role="presentation"
             className="background"
             // onClick={toggleDrawer(anchor, false)}
             // onKeyDown={toggleDrawer(anchor, false)}
         >
+            {console.log(PersonInfo)}
             <List className="row_side">
-                <Avatar alt="Remy Sharp" className="align-self-center" sx={{ width: 60, height: 60 }} />
-                <span className="lead fw-bold align-self-center mb-5">Ali Awan</span>
-                {['Personal Information', 'Shipping Area', 'Change Password',].map((text, index) => (
+                <Avatar alt="Remy Sharp" className="align-self-center mt-3" sx={{ width: 70, height: 70 }} />
+                <span className=" align-self-center mb-5 boldText">Ali Awan</span>
+                    <ListItem className="mx-3 mb-1 "
+                        button
+                    onClick={() => {
+                            setChangePassword(false);
+                            setpersonInfo(true);
+                            setshipping(false)
+                       
+                    }}
+                >
+                        <ListItemText
+                            disableTypography
+                        primary={<Typography type="body2" style={{ fontWeight: PersonInfo ? 'bold' : '' }}>Personal Information</Typography>} />
+                    </ListItem>
                     <ListItem className="mx-3 mb-1 boldText"
                         button
-                        activeClassName={classes.active}
-                        key={text}>
-                        <ListItemText primary={text} />
+                        onClick={() => {
+                                setChangePassword(false);
+                                setpersonInfo(false);
+                                setshipping(true)
+                    }}
+                >
+                        <ListItemText
+                            disableTypography
+                        primary={<Typography type="body2" style={{ fontWeight: ShippingArea ? 'bold' : '' }}>Shipping Area</Typography>} />
+                    </ListItem>
+                    <ListItem className="mx-3 mb-1 boldText"
+                        button
+                        onClick={(text) => {
+                                setChangePassword(true);
+                                setpersonInfo(false);
+                                setshipping(false)
+                    }}
+                >
+                        <ListItemText
+                        disableTypography
+                        primary={<Typography type="body2" style={{ fontWeight: ChangePassword ? 'bold' : '' }}>Change Password</Typography>} />
                     </ListItem>
                    
-                ))}
+                
                 <button type="button" class="side_btn btn  fw-bold my-4 ">Return Home</button>
             </List>
+            
             
         </Box>
     )
